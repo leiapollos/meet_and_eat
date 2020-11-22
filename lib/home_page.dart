6b5632444793dfc1fae9f7b0c185d6b38799a3d1
André <andreleite98@hotmail.com>
2020-11-22 14:35:39 +0000

    ez pz all praise toni kukoc

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,6 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:meet_and_eat/authentication_service.dart';
 import 'package:provider/provider.dart';
 import 'package:meet_and_eat/GetUsers.dart';
+import 'dart:developer';
 
 class HomePage extends StatelessWidget {
 
@@ -36,10 +37,24 @@ class HomePage extends StatelessWidget {
   }
 }
 
+class Profile {
+  final name;
+  final lastName;
+  Profile(this.name, this.lastName);
+
+  String GetName(){
+    return name;
+  }
+
+  String GetLastName(){
+    return lastName;
+  }
+
+}
+
 class DataSearch extends SearchDelegate<String>{
   /// TODO: USE BACKEND DATA AND UPDATE RECENTPEOPLE
-  final people = ['Toni Kukoc', 'SIUU', 'Daddy Andre'];
-  final recentPeople = ['Toni Kukoc']; // History
+
 
   @override
   List<Widget> buildActions(BuildContext context) {
@@ -64,43 +79,117 @@ class DataSearch extends SearchDelegate<String>{
   @override
   Widget buildResults(BuildContext context) {
     // Show result base on the selection
-    return Card(
+    /*return Card(
       color: Colors.blue,
       child: Center(
-        child: Text(query),
+        child: Text(profile[0]['lastName']),
       ),
-    );
+    );*/
+    final people = FirebaseFirestore.instance.collection(
+        'profiles').where('name',  isGreaterThanOrEqualTo: query.toString())
+        .where('name', isLessThan: query.toString() + 'z');
+
+    return StreamBuilder<QuerySnapshot>(
+        stream: people.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Loading");
+          }
+          final profiles = snapshot.data.docs;
+          return Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              //shrinkWrap: true,
+              itemCount: profiles.length,
+              itemBuilder: (context, index) =>
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.94,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      color: Colors.white,
+                      elevation: 3,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: ConstrainedBox(
+                              constraints: BoxConstraints(
+                                maxWidth: MediaQuery.of(context).size.width * 0.28,
+                                maxHeight: MediaQuery.of(context).size.width * 0.28,
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(200),
+                                child: (profiles[index]['url'] != null && profiles[index]['url'].toString().isNotEmpty)
+                                    ? Image.network(profiles[index]['url']) : Image.asset('assets/images/chimo.png', fit: BoxFit.fill),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: ListTile(title: Text(profiles[index]['name'] + " " + profiles[index]['lastName'], style: const TextStyle(fontSize: 19.0, fontWeight: FontWeight.w700),),
+                              subtitle: Padding(
+                                padding: const EdgeInsets.only(left: 0, top: 5, right: 0, bottom: 0),
+                                child: Text(profiles[index]['age'].toString(), style: const TextStyle(fontSize: 17.0, fontWeight: FontWeight.w500),),
+                              ),),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+            ),
+          );
+
+        });
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Suggestion search
-    final suggestionList = query.isEmpty
-        ? recentPeople
-        : people.where((element) => element.toLowerCase().startsWith(query)).toList();
-    
-    return ListView.builder(
-        itemBuilder: (contex, index) => ListTile(
+    final people = FirebaseFirestore.instance.collection(
+        'profiles').where('name',  isGreaterThanOrEqualTo: query.toString())
+        .where('name', isLessThan: query.toString() + 'z');
+
+    return StreamBuilder<QuerySnapshot>(
+        stream: people.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+        final profiles = snapshot.data.docs;
+      return ListView.builder(
+        itemBuilder: (context, index) => ListTile(
         onTap: (){//ON CLICK
           showResults(context);
         },
           leading: Icon(Icons.food_bank),
           title: RichText(
            text: TextSpan(
-               text: suggestionList[index].substring(0, query.length),
+               text: profiles[index]['name'] + " ",
                style: TextStyle(
                  color: Colors.black, fontWeight: FontWeight.bold),
                 children: [
                   TextSpan(
-                    text: suggestionList[index].substring(query.length),
+                    text: profiles[index]['lastName'],
                     style: TextStyle(color: Colors.grey)
                   )
                 ]
            ),
           ),
         ),
-      itemCount: suggestionList.length,
+      itemCount: profiles.length,
     );
-  }
+
+  });
   
+}
 }
