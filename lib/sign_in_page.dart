a27771e9838876af707dece06c6084a71c1f6311
André <andreleite98@hotmail.com>
2020-12-06 15:31:35 +0000

    prettied up search result for meals

--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -58,110 +58,124 @@ class _SignInPage extends State<SignInPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "assets/images/login_background.jpg",
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
+        ),
+        Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              height: 100.0,
-              child: Image.asset('assets/images/logo.png'),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-                contentPadding:
-                EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide:
-                  BorderSide(color: Color(0xff3d405b), width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide:
-                  BorderSide(color: Color(0xff3d405b), width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: 'Enter your password.',
-                contentPadding:
-                EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide:
-                  BorderSide(color: Color(0xff3d405b), width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide:
-                  BorderSide(color: Color(0xff3d405b), width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
-
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Color(0xff81b29a),
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    _logIn();
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',style: TextStyle(color: Colors.white, fontSize: 26.0, fontWeight:FontWeight.w300)
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0),
-              child: Material(
-                color: Color(0xff81b29a),
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    _register();
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register', style: TextStyle(color: Colors.white, fontSize: 26.0, fontWeight:FontWeight.w300)
-                  ),
-                ),
-              ),
-            ),
-          ],
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+        Container(
+        height: 100.0,
+        child: Image.asset('assets/images/logo.png'),
         ),
-      ),
+        SizedBox(
+        height: 48.0,
+        ),
+        TextField(
+        controller: emailController,
+        decoration: InputDecoration(
+          filled: true,
+        fillColor: Colors.white,
+        hintText: 'Enter your email',
+        contentPadding:
+        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+        border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(32.0)),
+        ),
+        enabledBorder: OutlineInputBorder(
+        borderSide:
+        BorderSide(color: Color(0xff3d405b), width: 1.5),
+        borderRadius: BorderRadius.all(Radius.circular(32.0)),
+        ),
+        focusedBorder: OutlineInputBorder(
+        borderSide:
+        BorderSide(color: Color(0xff3d405b), width: 2.0),
+        borderRadius: BorderRadius.all(Radius.circular(32.0)),
+        ),
+        ),
+        ),
+        SizedBox(
+        height: 8.0,
+        ),
+        TextField(
+        controller: passwordController,
+        obscureText: true,
+        decoration: InputDecoration(
+          filled: true,
+        fillColor: Colors.white,
+        hintText: 'Enter your password.',
+        contentPadding:
+        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+        border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(32.0)),
+        ),
+        enabledBorder: OutlineInputBorder(
+        borderSide:
+        BorderSide(color: Color(0xff3d405b), width: 1.5),
+        borderRadius: BorderRadius.all(Radius.circular(32.0)),
+        ),
+        focusedBorder: OutlineInputBorder(
+        borderSide:
+        BorderSide(color: Color(0xff3d405b), width: 2.0),
+        borderRadius: BorderRadius.all(Radius.circular(32.0)),
+        ),
+        ),
+
+        ),
+        SizedBox(
+        height: 24.0,
+        ),
+        Padding(
+        padding: EdgeInsets.symmetric(vertical: 16.0),
+        child: Material(
+        color: Color(0xff81b29a),
+        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+        elevation: 5.0,
+        child: MaterialButton(
+        onPressed: () {
+        _logIn();
+        },
+        minWidth: 200.0,
+        height: 42.0,
+        child: Text(
+        'Log In',style: TextStyle(color: Colors.white, fontSize: 26.0, fontWeight:FontWeight.w300)
+        ),
+        ),
+        ),
+        ),
+        Padding(
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        child: Material(
+        color: Color(0xff81b29a),
+        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+        elevation: 5.0,
+        child: MaterialButton(
+        onPressed: () {
+        _register();
+        },
+        minWidth: 200.0,
+        height: 42.0,
+        child: Text(
+        'Register', style: TextStyle(color: Colors.white, fontSize: 26.0, fontWeight:FontWeight.w300)
+        ),
+        ),
+        ),
+        ),
+        ],
+        ),
+        ),
+        ),
+      ],
     );
   }
 
