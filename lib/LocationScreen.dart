4335052dc8b23b4b17596e70ad76049e0f13362a
Adrian <adrianrodriguesm98@gmail.com>
2020-12-06 18:13:30 +0000

    Location.

--- lib/LocationScreen.dart
+++ lib/LocationScreen.dart
@@ -7,6 +7,8 @@ import 'package:meet_and_eat/ProfileScreen.dart';
 import 'package:meet_and_eat/authentication_service.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
+import 'package:geolocation/geolocation.dart';
+import 'package:flutter_map/flutter_map.dart';
 
 class LocationScreen extends StatefulWidget {
   final String uid;
@@ -17,14 +19,69 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreen extends State<LocationScreen>{
-  DateFormat _dateFormat;
+
+  MapController controller = new MapController();
+
+  getPermission() async {
+    final GeolocationResult result =
+        await Geolocation.requestLocationPermission(
+                permission: LocationPermission(
+                  android: LocationPermissionAndroid.fine
+                )
+             );
+
+    return result;
+  }
+
+  getLocation()
+  {
+    return getPermission().then((result) async{
+      if(result.isSuccessful){
+        final coords =
+              await Geolocation.currentLocation(accuracy: LocationAccuracy.best);
+      }
+    });
+  }
+
+  buildMap()
+  {
+    getLocation().then((response) {
+      if(response.isSuccessful){
+        response.listen((value) {
+          controller.move( latLng.LatLng(value.location.latitude,value.location.longitude), zoom);
+        });
+
+      }
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xff3d405b),
+    return new FlutterMap(
+      options: new MapOptions(
+        center: new latlng.LatLng(51.5, -0.09),
+        zoom: 13.0,
       ),
-      body: Text("hello"),
+      layers: [
+        new TileLayerOptions(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a', 'b', 'c']
+        ),
+        new MarkerLayerOptions(
+          markers: [
+            new Marker(
+              width: 80.0,
+              height: 80.0,
+              point: new LatLng(51.5, -0.09),
+              builder: (ctx) =>
+              new Container(
+                child: new FlutterLogo(),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
