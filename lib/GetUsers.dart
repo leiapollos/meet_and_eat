721fbd40e9a851e39aac3da784d52dc657421d65
André <andreleite98@hotmail.com>
2020-12-06 15:42:24 +0000

    date picker color

--- lib/GetUsers.dart
+++ lib/GetUsers.dart
@@ -9,11 +9,10 @@ import 'package:meet_and_eat/authentication_service.dart';
 import 'package:provider/provider.dart';
 
 class GetUsers extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    CollectionReference users = FirebaseFirestore.instance.collection(
-        'profiles');
+    CollectionReference users =
+        FirebaseFirestore.instance.collection('profiles');
     final uid = context.watch<AuthenticationService>().getUserId();
     return StreamBuilder<QuerySnapshot>(
       stream: users.snapshots(),
@@ -32,59 +31,84 @@ class GetUsers extends StatelessWidget {
               //shrinkWrap: true,
               itemCount: profiles.length,
               itemBuilder: (context, index) {
-                if(profiles[index].id == uid)
-                  return Center();
+                if (profiles[index].id == uid) return Center();
                 return FlatButton(
-                onPressed: () {
-                  //print(profiles[index].id);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ChatSystem(otherName: profiles[index]['name'], otheruid: profiles[index].id, myuid: uid,),),
-                  );
-                },
-                child: Container(
-
-                  //onPressed:() => {},
-                  width: MediaQuery.of(context).size.width * 0.94,
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    color: Colors.white,
-                    elevation: 3,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: ConstrainedBox(
-                            constraints: BoxConstraints(
-                              maxWidth: MediaQuery.of(context).size.width * 0.28,
-                              maxHeight: MediaQuery.of(context).size.width * 0.28,
-                            ),
-                            child: CircleAvatar(
-                              radius: 45,
-                              backgroundImage: (profiles[index]['url'] != null && profiles[index]['url'].toString().isNotEmpty)
-                                  ? NetworkImage(profiles[index]['url']) : NetworkImage("https://cdn.pixabay.com/photo/2013/07/13/12/07/avatar-159236_640.png"),
-                              backgroundColor: Colors.blue,
+                  onPressed: () {
+                    //print(profiles[index].id);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChatSystem(
+                          otherName: profiles[index]['name'],
+                          otheruid: profiles[index].id,
+                          myuid: uid,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    //onPressed:() => {},
+                    width: MediaQuery.of(context).size.width * 0.94,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      color: Colors.white,
+                      elevation: 3,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: ConstrainedBox(
+                              constraints: BoxConstraints(
+                                maxWidth:
+                                    MediaQuery.of(context).size.width * 0.28,
+                                maxHeight:
+                                    MediaQuery.of(context).size.width * 0.28,
+                              ),
+                              child: CircleAvatar(
+                                radius: 45,
+                                backgroundImage: (profiles[index]['url'] !=
+                                            null &&
+                                        profiles[index]['url']
+                                            .toString()
+                                            .isNotEmpty)
+                                    ? NetworkImage(profiles[index]['url'])
+                                    : NetworkImage(
+                                        "https://cdn.pixabay.com/photo/2013/07/13/12/07/avatar-159236_640.png"),
+                                backgroundColor: Colors.blue,
+                              ),
                             ),
                           ),
-                        ),
-                        Expanded(
-                          child: ListTile(title: Text(profiles[index]['name'] + " " + profiles[index]['lastName'], style: const TextStyle(fontSize: 19.0, fontWeight: FontWeight.w700),),
-                            subtitle: Padding(
-                              padding: const EdgeInsets.only(left: 0, top: 5, right: 0, bottom: 0),
-                              child: Text(profiles[index]['age'].toString(), style: const TextStyle(fontSize: 17.0, fontWeight: FontWeight.w500),),
-                            ),),
-                        ),
-                      ],
+                          Expanded(
+                            child: ListTile(
+                              title: Text(
+                                profiles[index]['name'] +
+                                    " " +
+                                    profiles[index]['lastName'],
+                                style: const TextStyle(
+                                    fontSize: 19.0,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                              subtitle: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 0, top: 5, right: 0, bottom: 0),
+                                child: Text(
+                                  profiles[index]['age'].toString(),
+                                  style: const TextStyle(
+                                      fontSize: 17.0,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              );
-              }
-
-          ),
+                );
+              }),
         );
       },
     );
